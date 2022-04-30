d57cfddd20e8cfe7c9cbb4825fbd8ef6d093d64c
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-04-30 13:00:20 -0300

    feat-front: criando o roteamento das paginas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DashboardPage from './Pages/DashboardPage';
+import MayorRegisterPage from './Pages/MayorRegisterPage';
+import VotePage from './Pages/VotePage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<VotePage />} />
+      <Route path="/register" element={<MayorRegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
   );
 }
 
