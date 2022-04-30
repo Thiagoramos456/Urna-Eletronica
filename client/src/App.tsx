2b9cbba937031cddad1f78ff1c6c3c2edd5f7b6a
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-04-30 13:09:50 -0300

    feat-front: criando o header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
 import DashboardPage from './Pages/DashboardPage';
 import MayorRegisterPage from './Pages/MayorRegisterPage';
 import VotePage from './Pages/VotePage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<VotePage />} />
-      <Route path="/register" element={<MayorRegisterPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<VotePage />} />
+        <Route path="/register" element={<MayorRegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </div>
   );
 }
 
